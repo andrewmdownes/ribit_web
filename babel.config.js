bba1756f7cf7a0211b047c7f348b08ed00c0bc63
andrewmdownes <156127887+andrewmdownes@users.noreply.github.com>
2025-09-02 22:05:51 -0500

    fix: correctly configure babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
-// babel.config.js
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ["module:react-native-dotenv"]
+      ["module:react-native-dotenv"],
+      'react-native-reanimated/plugin',
     ]
   };
 };
